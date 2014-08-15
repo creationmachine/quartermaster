cd851a701b704a6a3931acda23beac126e08ff7c
Chris Haveard <chris@creationmachine.com>
2014-08-15 14:47:25 -0500

    Update Palindrome.java

--- java/examples/Palindrome.java
+++ java/examples/Palindrome.java
@@ -22,3 +22,43 @@ class Palindrome
  
    }
 }	
+
+
+/* One Liner
+
+public static boolean oneLinerPalin(String str){
+    return str.equals(new StringBuffer(str).reverse().toString());
+}
+
+*/
+
+/* No StringBuffer
+
+public static boolean isPalindrome(String str){
+
+    if(str.isEmpty()) return true;
+
+    int last = str.length() - 1;        
+
+    for(int i = 0; i <= last / 2;i++)
+        if(str.charAt(i) != str.charAt(last - i))
+            return false;
+
+    return true;
+}
+
+*/
+
+/* Recursive
+
+public static boolean recursivePalin(String str){
+    return check(str, 0, str.length() - 1);
+}
+
+private static boolean check (String str,int start,int stop){
+    return stop - start < 2 ||
+           str.charAt(start) == str.charAt(stop) &&
+           check(str, start + 1, stop - 1);
+}
+
+*/
